54dc28f0aeab1bf79b26049100463577a6181fc2
ryan2156 <adasdsd60112@gmail.com>
2020-06-02 19:30:17 +0800

    no message

--- car.py
+++ car.py
@@ -1,4 +1,15 @@
-for i in range(1,6):
-    for j in range(1,6):
+x=int(input('輸入層數'))
+
+for i in range(1,x+1):
+    for k in range(i,x):
+        print(" ",end='')
+    for j in range(1,2*i):
         print('*',end='')
     print("")
+
+for i in range(0,x):
+    for k in range(1,x-1):
+        print(' ',end='')
+    for j in range(0,3):
+        print('*',end='')
+    print('')
