a7b562fe8cc03d006cc2afe08d2075a7223a44c6
ryan2156 <64963540+ryan2156@users.noreply.github.com>
2020-06-15 20:00:10 +0800

    Update car.py

--- car.py
+++ car.py
@@ -1,4 +1,17 @@
-for i in range(1,6):
-    for j in range(1,6):
-        print('*',end='')
-    print("")
+#def gcd(m,n):
+ #   if n== 0:
+   #     return m
+  #  else:
+    #    return(n,m%n)
+    #print(gcd(20,30))
+
+class jojo():
+    age = "27"
+    def talk(self):
+        return "hellow"
+
+j = jojo() # 宣告成新物件
+
+print(jojo.age)
+print(j.talk())      
+
